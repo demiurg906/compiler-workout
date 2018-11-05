9e0fedc994bf301c3c4ed03ab649de8a013e8e7a
Dmitriy Novozhilov <novodmit.2x@yandex.ru>
2018-11-05 18:05:18 +0300

    hw7

--- src/SM.ml
+++ src/SM.ml
@@ -14,7 +14,7 @@ open Language
 (* conditional jump                *) | CJMP  of string * string
 (* begins procedure definition     *) | BEGIN of string list * string list
 (* end procedure definition        *) | END
-(* calls a procedure               *) | CALL  of string with show
+(* calls a procedure               *) | CALL of string with show
                                                    
 (* The type for the stack machine program *)                                                               
 type prg = insn list
@@ -30,8 +30,57 @@ type config = (prg * State.t) list * int list * Expr.config
 
    Takes an environment, a configuration and a program, and returns a configuration as a result. The
    environment is used to locate a label to jump to (via method env#labeled <label_name>)
-*)                                                  
-let rec eval env ((cstack, stack, ((st, i, o) as c)) as conf) prg = failwith "Not implemented"
+*)
+let rec eval env ((call_stack, stack, ((state, input, output) as conf)) as config) prog = match prog with
+  | [] -> config
+  | inst :: prog -> match inst with
+      | JMP label -> eval env config (env#labeled label)
+      | CJMP (op, label) -> 
+        let var::stack' = stack in 
+        let config' = (call_stack, stack', conf) in
+        let predicate = match op with
+          | "z"  -> (==) 0
+          | "nz" -> (!=) 0
+        in if predicate var
+            then eval env config' (env#labeled label)
+            else eval env config' prog     
+
+      | CALL func -> eval env ((prog, state)::call_stack, stack, conf) (env#labeled func)
+      
+      | END -> 
+        let (prog, state')::call_stack' = call_stack
+        in eval env (call_stack', stack, (State.leave state state', input, output)) prog
+
+      | _ ->
+        let config' = begin match inst with
+          | BINOP op -> 
+            let right::left::stack' = stack
+            in (call_stack, (Language.Expr.eval_binop op left right) :: stack', conf)
+          | CONST x -> (call_stack, x :: stack, conf)
+          | READ ->
+            let x :: input' = input
+            in (call_stack, x :: stack, (state, input', output))
+          | WRITE ->
+            let x :: stack' = stack
+            in (call_stack, stack', (state, input, output @ [x]))
+          | LD var ->
+            let value = State.eval state var
+            in (call_stack, value :: stack, conf)
+          
+          | ST var ->
+            let x :: stack' = stack
+            in (call_stack, stack', (State.update var x state, input, output))
+          
+          | LABEL label -> config
+          
+          | BEGIN (p, l) ->
+            let new_state = State.enter state (p @ l) in
+            let (state', stack') = List.fold_right (
+                fun p (st, x::stack') -> (State.update p x st, stack')  
+              ) p (new_state, stack) 
+            in (call_stack, stack', (state', input, output))
+        end
+        in eval env config' prog
 
 (* Top-level evaluation
 
@@ -50,6 +99,14 @@ let run p i =
   let m = make_map M.empty p in
   let (_, _, (_, _, o)) = eval (object method labeled l = M.find l m end) ([], [], (State.empty, i, [])) p in o
 
+class label_manager =
+ object (self)
+   val mutable label = 0
+
+   method new_label = let last_label = label in
+     label <- label + 1; Printf.sprintf "L%d" last_label
+end
+
 (* Stack machine compiler
 
      val compile : Language.t -> prg
@@ -57,4 +114,63 @@ let run p i =
    Takes a program in the source language and returns an equivalent program for the
    stack machine
 *)
-let compile (defs, p) = failwith "Not implemented"
+let rec compile (defs, prog) =
+  let rec compile_stmt label_manager =
+    let rec compile_expr = function
+    | Expr.Var   x          -> [LD x]
+    | Expr.Const n          -> [CONST n]
+    | Expr.Binop (op, x, y) -> compile_expr x @ compile_expr y @ [BINOP op]
+    | Expr.Call (func_name, args) -> List.concat (List.map compile_expr args) @ [CALL func_name]
+    in
+    function
+    | Stmt.Seq (s1, s2)  -> compile_stmt label_manager s1 @ compile_stmt label_manager s2
+    | Stmt.Read x        -> [READ; ST x]
+    | Stmt.Write e       -> compile_expr e @ [WRITE]
+    | Stmt.Assign (x, e) -> compile_expr e @ [ST x]
+    | Stmt.Skip -> []
+    | Stmt.If (cond, then_block, else_block) -> 
+      let else_label = label_manager#new_label in
+      let end_label  = label_manager#new_label
+      in compile_expr cond
+        @ [CJMP ("z", else_label)] 
+        @ compile_stmt label_manager then_block 
+        @ [JMP end_label; LABEL else_label] 
+        @ compile_stmt label_manager else_block
+        @ [LABEL end_label]
+    
+    | Stmt.While (cond, block) ->
+      let loop_label = label_manager#new_label in
+      let end_label  = label_manager#new_label 
+      in [LABEL loop_label] 
+        @ compile_expr cond
+        @ [CJMP ("z", end_label)]
+        @ compile_stmt label_manager block
+        @ [JMP loop_label; LABEL end_label]
+    
+    | Stmt.Repeat (cond, block) ->
+      let loop_label = label_manager#new_label
+      in [LABEL loop_label]
+        @ compile_stmt label_manager block
+        @ compile_expr cond
+        @ [CJMP ("z", loop_label)]
+    
+    | Stmt.Call (func_name, args) -> List.concat (List.map compile_expr args) @ [CALL func_name]
+    | Stmt.Return return_value -> (match return_value with 
+        | None -> [] 
+        | Some expr -> compile_expr expr
+      ) @ [END]
+  in
+
+  let compile_procedure label_manager (func_name, (args, local_vars, body)) =
+    [LABEL func_name; BEGIN (args, local_vars)] 
+    @ compile_stmt label_manager body
+    @ [END]
+  in
+  
+  let label_manager = new label_manager in 
+  let main_label = label_manager#new_label
+  
+  in [JMP main_label]
+   @ List.concat (List.map (compile_procedure label_manager) defs)
+   @ [LABEL main_label]
+   @ compile_stmt label_manager prog
